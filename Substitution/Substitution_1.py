b99732c0663d0a3ab676bf2d7b66555bf5ace769
Jalynne-YH <lvur_@naver.com>
2020-04-30 22:30:59 +0900

    Upload Substitution Cipher

---
+++ Substitution/Substitution_1.py
@@ -0,0 +1,37 @@
+message = "ogrk hg tl`n srbszizrzig aiphgu mg ycahzylluf vllu fg ahcoogneg pceinc is ngzdluag"
+
+# ogrk hg tl`n srbsz → leuk he zo`n subst"
+
+message = message.replace('o', 'L')
+message = message.replace('g', 'E')
+message = message.replace('r', 'U')
+message = message.replace('k', 'K')
+message = message.replace('h', 'H')
+message = message.replace('t', 'Z')
+message = message.replace('l', 'O')
+message = message.replace('n', 'N')
+message = message.replace('s', 'S')
+message = message.replace('b', 'B')
+message = message.replace('z', 'T')
+
+message = message.replace('e', 'G')
+message = message.replace('u', 'R')
+
+# ahcLLEnGE → CHALLENGE
+message = message.replace('a', 'C')
+message = message.replace('c', 'A')
+
+# CIpHER → CIPHER
+message = message.replace('p', 'P')
+
+# iS → IS
+message = message.replace('i', 'I')
+
+# fE → DE / NETdORCE → NETFORCE
+message = message.replace('f', 'D')
+message = message.replace('d', 'F')
+
+# yACHTyOORD → WACHTWOORD (password)
+message = message.replace('y', 'W')
+
+print(message)
